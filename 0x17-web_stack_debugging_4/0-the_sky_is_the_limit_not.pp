4a04c1739e7a391b7c5758e3c91977828ba1cdcd
Kimberly Wong <kjowong@gmail.com>
2017-09-16 13:28:25 +0000

    Adding -i in sed

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # Using Puppet to up the ULIMIT in the /etc/default/nginx file
 exec { 'update_ulimit':
-      command =>  'sed \'s/-n 15/-n 15000/\' /etc/default/nginx',
+      command =>  'sed -i \'s/-n 15/-n 15000/\' /etc/default/nginx'\
+                  '&& service nginx restart',
       path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
 
