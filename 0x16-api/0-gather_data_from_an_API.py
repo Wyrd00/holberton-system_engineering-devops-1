59a38396e7eda2560ec56f8f56dcceaf663a4f08
Kimberly Wong <kjowong@gmail.com>
2017-09-16 02:18:01 +0000

    Keep quotes consistent

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -9,12 +9,12 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) > 1:
         user = argv[1]
-        api_url = 'https://jsonplaceholder.typicode.com/'
-        req_user = requests.get('{}users/{}'.format(api_url, user))
+        api_url = "https://jsonplaceholder.typicode.com/"
+        req_user = requests.get("{}users/{}".format(api_url, user))
         name = req_user.json().get("name")
         if name is not None:
             json_req = requests.get(
-                '{}todos?userId={}'.format(
+                "{}todos?userId={}".format(
                     api_url, user)).json()
             all_task = len(json_req)
             completed_task = []
@@ -25,4 +25,4 @@ if __name__ == "__main__":
             print("Employee {} is done with tasks({}/{}):"
                   .format(name, completed_count, all_task))
             for title in completed_task:
-                print('\t {}'.format(title.get('title')))
+                print("\t {}".format(title.get("title")))
--- 0x16-api/1-export_to_CSV.py
+++ 0x16-api/1-export_to_CSV.py
@@ -9,17 +9,17 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) > 1:
         user = argv[1]
-        api_url = 'https://jsonplaceholder.typicode.com/'
-        req_user = requests.get('{}users/{}'.format(api_url, user))
+        api_url = "https://jsonplaceholder.typicode.com/"
+        req_user = requests.get("{}users/{}".format(api_url, user))
         username = req_user.json().get("username")
         all_tasks = requests.get(
-            '{}todos?userId={}'.format(
+            "{}todos?userId={}".format(
                 api_url, user)).json()
-        with open("{}.csv".format(user), 'w') as csvfile:
+        with open("{}.csv".format(user), "w") as csvfile:
             writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
             for task in all_tasks:
                 writer.writerow(
-                    (task.get('userId'),
+                    (task.get("userId"),
                      username,
-                     task.get('completed'),
-                        task.get('title')))
+                     task.get("completed"),
+                        task.get("title")))
