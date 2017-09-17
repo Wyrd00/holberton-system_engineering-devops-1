87db13209b17b19d0ec0021f54aee3690df7fee0
Kimberly Wong <kjowong@gmail.com>
2017-09-17 09:22:00 +0000

    Fixing condition for after

--- 0x18-api_advanced/2-recurse.py
+++ 0x18-api_advanced/2-recurse.py
@@ -4,7 +4,6 @@
     containing the titles of all hot articles for a given subreddit
 """
 import requests
-import pprint
 
 
 def recurse(subreddit, hot_list=[], after=None):
@@ -13,17 +12,22 @@ def recurse(subreddit, hot_list=[], after=None):
         given subreddit
         If no results are found, return None
     """
-    reddit_url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    if after is None:
+        b_url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(
+            subreddit)
+    b_url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".format(
+        subreddit, after)
     headers = {"User-Agent": "Holberton API access v0.1 (by /u/holbie)"}
-    reddit_response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+    reddit_response = requests.get(
+        b_url,
+        headers=headers,
+        allow_redirects=False)
     if reddit_response.status_code is not 200:
         return None
-    else:
-        reddit_resp_json = reddit_response.json()
-        pp = pprint.PrettyPrinter(indent=4)
-        children = reddit_resp_json.get("data").get("children")
-        hot_list += children
-        after = reddit_resp_json.get("data").get("after")
-        if after:
-            reddit_url = "https://www.reddit.com/r/{}/hot.json?after={}?limit=100".format(subreddit, after)
-            recurse(subreddit, hot_list, after)
+    reddit_resp_json = reddit_response.json()
+    children = reddit_resp_json.get("data").get("children")
+    hot_list += children
+    after = reddit_resp_json.get("data").get("after")
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
