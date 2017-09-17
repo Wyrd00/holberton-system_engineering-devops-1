683ef24c85ec22e84c43f71eb5fa127632a1b795
Kimberly Wong <kjowong@gmail.com>
2017-09-17 04:09:21 +0000

    mend
    Initial commit

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Using Puppet to up the ULIMIT in the /etc/default/nginx file
 exec { 'update_ulimit':
-      command =>  'sed -i \'s/-n 15/-n 15000/\' /etc/default/nginx && service nginx restart',
+      command =>  'sed -i \'s/15/15000/\' /etc/default/nginx && service nginx restart',
       path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
 
